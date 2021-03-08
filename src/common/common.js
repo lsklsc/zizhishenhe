b2a3557b4fceaf38732aeb0801c8076227d7a402
liushoucheng <liushoucheng@ztbory.com>
2021-03-08 13:47:24 +0800

    .

--- src/common/common.js
+++ src/common/common.js
@@ -1,15 +1,16 @@
 import hint from '@/common/hint'
 //baseUrl本地地址
 // export const baseUrl = 'http://172.16.1.15:9009' // 本地地址/
-export const baseUrl ='https://api.ztbory.com/dev/qualification' // 测试环境
+// export const baseUrl ='https://api.ztbory.com/dev/qualification' // 测试环境
+export const baseUrl ='https://api.ztbory.com/v3/qualification' // 正式环境
 
 // devUrl测试地址
-export const devUrl ='https://api.ztbory.com/dev/qualification' // 测试环境
-export const imgUrl = 'https://api.ztbory.com/dev' // 测试图片/
+// export const devUrl ='https://api.ztbory.com/dev/qualification' // 测试环境
+// export const imgUrl = 'https://api.ztbory.com/dev' // 测试图片/
 
 // devUrl生产地址
-// export const devUrl ='https://api.ztbory.com/v3' // 生产环境
-// export const imgUrl = 'https://api.ztbory.com/v3' // 线上图片/
+export const devUrl ='https://api.ztbory.com/v3/qualification' // 生产环境
+export const imgUrl = 'https://api.ztbory.com/v3' // 线上图片/
 
 // 过滤数据
 export function filterData(newData, oldData, seleModel, seachModel) {
